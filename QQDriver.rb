ea42a4982731f2611e232e2d4a8c7aeaf58572ac
Bryce Thuilot <bthuilot@gmail.com>
2018-07-08 15:50:29 -0300

    More additions to web driver

--- QQDriver.rb
+++ QQDriver.rb
@@ -1,20 +1,25 @@
 require 'selenium-webdriver'
 
+# Used for testing
+policy_num = "CCH016155-04"
+name = "Mary Mill" 
+premium = "666"
+
 #Opens up QQ
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "https://app.qqcatalyst.com/Contacts/Search"
 
 sleep(2)
 
-policy_num = "CCH016155-04"
-name = "Mary Mill" 
+puts "Please enter password:"
+pass = gets.chomp
 
 if driver.current_url.include? "login.qqcatalyst.com"
     #Logs in
     emailField = driver.find_element(name: 'txtEmail')
     emailField.send_keys "dean@millcreekagency.com"
     passField = driver.find_element(id: 'txtPassword')
-    passField.send_keys "Mill1225"
+    passField.send_keys pass 
     driver.find_element(id: "lnkSubmit").click
 
     sleep(2)
@@ -41,11 +46,11 @@ links.each do |link|
     end
 end
 
-sleep(4)
+sleep(3)
 
 driver.find_element(id: "tab-Policies").click
 
-sleep(4)
+sleep(3)
 
 divs = driver.find_elements(tag_name: "div")
 
@@ -56,6 +61,96 @@ divs.each do |div|
     end
 end
 
+sleep(2)
 
+divs = driver.find_elements(class: "Label")
+
+divs.each do |policy_action|
+    if policy_action.attribute("innerHTML") == "Policy Action" and policy_action.displayed?
+        policy_action.click
+        policy_action.click
+        puts "Ran"
+    end
+end
+
+sleep(1)
+
+renewal_button = driver.find_elements(class: "PolicyActionRenew")
+
+renewal_button[0].click
+
+sleep(2)
+
+submit_yes = driver.find_elements(class: "yes")
+
+
+submit_yes.each do |submit_button|
+    if submit_button.attribute("innerHTML") == "Yes" and submit_button.displayed?
+        submit_button.click
+        break
+    end
+end
+
+until driver.current_url.include? "New=true&Mode=basic&extra=issue" and driver.current_url.include? "https://app.qqcatalyst.com/Policies/Policy/Details"do
+    sleep(2)
+end
+
+sleep(2)
+
+
+driver.find_element(id: "pb-PolicyInfo").click
+
+driver.find_element(name: "PolicyNo").send_keys policy_num
+
+driver.find_element(id: "pb-PolicyQuotes").click
+
+quote_info = driver.find_elements(class: "quotecarrier")
+
+quote_info.each do |quote|
+    if quote.element(:title) == "Ocean Harbor Casualty"
+        quote.click
+    end
+end
+
+
+quote_info = driver.find_elements(class: "context-default-edit")
+
+quote_info.each do |quote|
+    if quote.attribute("innerHTML") == "Edit"
+        quote.click
+    end
+end
+
+sleep(2)
+
+driver.find_element(:name "basePremium").send_keys premium 
+
+driver.find_element(:name "QuoteSubStatus").find_element(:css,"option[value='S']").click
+
+
+submit_yes = driver.find_elements(class: "section_save")
+
+
+submit_yes.each do |submit_button|
+    if submit_button.attribute("innerHTML") == "Save" and submit_button.displayed?
+        submit_button.click
+        break
+    end
+end
+
+
+sleep(2)
+
+driver.find_element(id: "pb-HomeLiabilityLimitsHOME").click
+
+submit_yes = driver.find_elements(class: "tabular-cell")
+
+
+submit_yes.each do |submit_button|
+    if submit_button.attribute("innerHTML") == "1" and submit_button.displayed?
+        submit_button.click
+        break
+    end
+end
 
 
